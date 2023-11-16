4376007d52004f9522a285c5c4dd1ebdb67a17f0
Mirek Mikus <kiiv@seznam.cz>
2023-11-16 11:15:25 +0100

    Progmem test

--- xnano-416/main.cpp
+++ xnano-416/main.cpp
@@ -4,6 +4,14 @@
 #include <avr/xmega.h>
 #include <util/delay.h>
 
+#include <avr/pgmspace.h>
+
+
+PROGMEM const char pgmString[] = "xyz";
+
+const char constString[] = "abcd";
+
+
 int main(void) {
     _PROTECTED_WRITE(CLKCTRL.MCLKCTRLA, CLKCTRL_CLKOUT_bm | CLKCTRL_CLKSEL_OSC20M_gc);
     _PROTECTED_WRITE(CLKCTRL.MCLKCTRLB, 0);
@@ -12,6 +20,10 @@ int main(void) {
 
     while (1) {
         _delay_ms(500);
+        
+        volatile uint16_t sum = (uint16_t)&pgmString;
+        sum = (uint16_t)&constString;
+        (void) sum;
         //VPORTB.IN = PIN5_bm; // toggle
     }
 }
